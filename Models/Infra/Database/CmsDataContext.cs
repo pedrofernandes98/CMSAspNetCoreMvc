b32170bddd20e83b491689f99d50b032df379c4e
Pedro Fernandes <ppedrohfs@gmail.com>
2022-07-09 18:59:43 -0300

    First

---
+++ Models/Infra/Database/CmsDataContext.cs
@@ -0,0 +1,24 @@
+
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+
+namespace cmsMVC.Models.Infra.Database
+{
+    public class CmsDataContext : DbContext
+    {
+        public CmsDataContext(DbContextOptions<CmsDataContext> options): base(options)
+        {
+            
+        }
+
+        public CmsDataContext() 
+        {       
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // JToken configuration = JToken.Parse(File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "apsettings.json")));
+            // optionsBuilder.UseSqlServer(configuration["ConnectionStrings"].ToString());
+        }
+    }
+}
